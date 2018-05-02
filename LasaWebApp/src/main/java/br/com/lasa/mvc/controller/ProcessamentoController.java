f12c2f2bed6c82e5efefecadbbfd407e2f7036d2
hugo.guimaraes <hugo.guimaraes@Alugacom-PC21.MJV.net>
2018-05-02 18:26:52 -0300

    Ajusta na inclusao de venda

---
+++ LasaWebApp/src/main/java/br/com/lasa/mvc/controller/ProcessamentoController.java
@@ -0,0 +1,10 @@
+package br.com.lasa.mvc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lasaRest")
+public class ProcessamentoController {
+
+}
--- LasaWebApp/src/main/java/br/com/lasa/mvc/controller/VendaController.java
+++ LasaWebApp/src/main/java/br/com/lasa/mvc/controller/VendaController.java
@@ -6,11 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,7 +18,7 @@ import br.com.lasa.mvc.entity.Venda;
 import br.com.lasa.mvc.service.IVendaService;
 
 @RestController
-@RequestMapping("/endpoint")
+@RequestMapping("/lasaRest")
 public class VendaController {
 	@Autowired
 	private IVendaService vendaService;
@@ -37,11 +35,6 @@ public class VendaController {
 		return new ResponseEntity<List<Venda>>(list, HttpStatus.OK);
 	}
 	
-	@GetMapping("venda/next")
-	public ResponseEntity<Venda> getNextOldVenda() {
-		Venda venda = vendaService.getNextOldVenda();
-		return new ResponseEntity<Venda>(venda, HttpStatus.OK);
-	}	
 	
 	@PostMapping("venda")
 	public ResponseEntity<Void> addVenda(@RequestBody Venda venda, UriComponentsBuilder builder) {
@@ -54,16 +47,5 @@ public class VendaController {
                 return new ResponseEntity<Void>(headers, HttpStatus.CREATED);
          }
 	}
-	
-	@PutMapping("venda")
-	public ResponseEntity<Venda> updateVenda(@RequestBody Venda venda) {
-		vendaService.updateVenda(venda);
-		return new ResponseEntity<Venda>(venda, HttpStatus.OK);
-	}
-	
-	@DeleteMapping("venda/{id}")
-	public ResponseEntity<Void> deleteVenda(@PathVariable("id") Integer id) {
-		vendaService.deleteVenda(id.longValue());
-		return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
-	}	
+		
 }
